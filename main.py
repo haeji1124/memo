e471bbde0dc42f622ccf7bd9ffdb21e20acf3fd6
haeji1124 <46364329+haeji1124@users.noreply.github.com>
2024-11-11 15:05:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,34 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+
 class Memo(BaseModel):
     id: str
     content: str
 
+
 memos = []
+
 
 @app.get("/memos")
 def read_memo():
     return memos
 
+
 @app.post("/memos")
 def create_memo(memo: Memo):
     memos.append(memo)
     return '메모 추가에 성공했습니다.'
+
+
+@app.put("/memos/{memo_id}")
+def edit_memo(req_memo: Memo):
+    for memo in memos:
+        if memo.id == req_memo.id:
+            memo.content = req_memo.content
+            return '메모 수정을 성공했습니다.'
+
+    return f'{req_memo.id}의 메모는 없습니다.'
+
 
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
