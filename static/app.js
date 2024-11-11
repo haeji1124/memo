59f889359765b8e03ed1de86ddf131f5f430347e
haeji1124 <46364329+haeji1124@users.noreply.github.com>
2024-11-11 10:25:07 +0900

    init

---
+++ static/app.js
@@ -0,0 +1,25 @@
+async function createMemo(value){
+    const res = await fetch("/memos", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            id: new Date(),
+            content: value
+        }),
+    });
+
+    const jsonRes = await res.json();
+    console.log(jsonRes)
+}
+
+function handleSubmit(e){
+    e.preventDefault();
+    const input = document.querySelector('#memo-input')
+    createMemo(input.value)
+    input.value=''
+}
+
+const form = document.querySelector('#memo-form')
+form.addEventListener("submit", handleSubmit)
